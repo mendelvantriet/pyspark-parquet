ab49404d33f6efc22bc6df54b87bb345e0539e0b
Mendel van 't Riet <mendel@vantriet.nl>
2022-01-05 11:08:57 +0100

    Set spark config to be used for pyarrow

--- src/python_script.py
+++ src/python_script.py
@@ -4,6 +4,10 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
   .master(os.environ.get('MASTER')) \
   .appName(os.environ.get('APP_NAME')) \
+  .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
+  .config("spark.sql.execution.arrow.pyspark.fallback.enabled", "true") \
+  .config("spark.sql.parquet.mergeSchema", "false") \
+  .config("spark.hadoop.parquet.enable.summary-metadata", "false") \
   .getOrCreate()
         
 df = spark.read.parquet(os.environ.get('INPUT_FILE'))
